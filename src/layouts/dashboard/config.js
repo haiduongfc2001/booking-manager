7af5f4ef1081b232444b1713665f7ef381d97e26
haiduongfc2001 <haiduongfc2001@gmail.com>
2024-02-02 01:06:13 +0700

    dhd-004: refactor code side nav component

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,84 +1,84 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
-import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
-import UserIcon from '@heroicons/react/24/solid/UserIcon';
-import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
-import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
-import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
-import { SvgIcon } from '@mui/material';
+import ChartBarIcon from "@heroicons/react/24/solid/ChartBarIcon";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import LockClosedIcon from "@heroicons/react/24/solid/LockClosedIcon";
+import UserIcon from "@heroicons/react/24/solid/UserIcon";
+import UserPlusIcon from "@heroicons/react/24/solid/UserPlusIcon";
+import UsersIcon from "@heroicons/react/24/solid/UsersIcon";
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
+import { SvgIcon } from "@mui/material";
+import HotelIcon from "@mui/icons-material/Hotel";
 
 export const items = [
   {
-    title: 'Overview',
-    path: '/',
+    title: "Tổng quan",
+    path: "/",
     icon: (
       <SvgIcon fontSize="small">
         <ChartBarIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Customers',
-    path: '/customers',
+    title: "Khách hàng",
+    path: "/customers",
     icon: (
       <SvgIcon fontSize="small">
         <UsersIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Companies',
-    path: '/companies',
+    title: "Khách sạn",
+    path: "/companies",
     icon: (
       <SvgIcon fontSize="small">
-        <ShoppingBagIcon />
+        <HotelIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Account',
-    path: '/account',
+    title: "Tài khoản",
+    path: "/account",
     icon: (
       <SvgIcon fontSize="small">
         <UserIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Settings',
-    path: '/settings',
+    title: "Cài đặt",
+    path: "/settings",
     icon: (
       <SvgIcon fontSize="small">
         <CogIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Login',
-    path: '/auth/login',
+    title: "Đăng nhập",
+    path: "/auth/login",
     icon: (
       <SvgIcon fontSize="small">
         <LockClosedIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Register',
-    path: '/auth/register',
+    title: "Đăng ký",
+    path: "/auth/register",
     icon: (
       <SvgIcon fontSize="small">
         <UserPlusIcon />
       </SvgIcon>
-    )
+    ),
   },
   {
-    title: 'Error',
-    path: '/404',
+    title: "Error",
+    path: "/404",
     icon: (
       <SvgIcon fontSize="small">
         <XCircleIcon />
       </SvgIcon>
-    )
-  }
+    ),
+  },
 ];
--- src/layouts/dashboard/side-nav.js
+++ src/layouts/dashboard/side-nav.js
@@ -1,45 +1,44 @@
-import NextLink from 'next/link';
-import { usePathname } from 'next/navigation';
-import PropTypes from 'prop-types';
-import ArrowTopRightOnSquareIcon from '@heroicons/react/24/solid/ArrowTopRightOnSquareIcon';
-import ChevronUpDownIcon from '@heroicons/react/24/solid/ChevronUpDownIcon';
-import {
-  Box,
-  Button,
-  Divider,
-  Drawer,
-  Stack,
-  SvgIcon,
-  Typography,
-  useMediaQuery
-} from '@mui/material';
-import { Logo } from 'src/components/logo';
-import { Scrollbar } from 'src/components/scrollbar';
-import { items } from './config';
-import { SideNavItem } from './side-nav-item';
+import NextLink from "next/link";
+import { usePathname } from "next/navigation";
+import PropTypes from "prop-types";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ChevronUpDownIcon from "@heroicons/react/24/solid/ChevronUpDownIcon";
+import { Box, Divider, Drawer, Stack, SvgIcon, Typography, useMediaQuery } from "@mui/material";
+import { Logo } from "src/components/logo";
+import { Scrollbar } from "src/components/scrollbar";
+import { items } from "./config";
+import { SideNavItem } from "./side-nav-item";
+import { useRouter } from "next/router";
+import { useAuth } from "src/hooks/use-auth";
 
 export const SideNav = (props) => {
   const { open, onClose } = props;
+  const auth = useAuth();
+  const router = useRouter();
   const pathname = usePathname();
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
+
+  const handleSignOut = () => {
+    router.push("/auth/login");
+  };
 
   const content = (
     <Scrollbar
       sx={{
-        height: '100%',
-        '& .simplebar-content': {
-          height: '100%'
+        height: "100%",
+        "& .simplebar-content": {
+          height: "100%",
         },
-        '& .simplebar-scrollbar:before': {
-          background: 'neutral.400'
-        }
+        "& .simplebar-scrollbar:before": {
+          background: "neutral.400",
+        },
       }}
     >
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%'
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
         }}
       >
         <Box sx={{ p: 3 }}>
@@ -47,67 +46,58 @@ export const SideNav = (props) => {
             component={NextLink}
             href="/"
             sx={{
-              display: 'inline-flex',
+              display: "inline-flex",
               height: 32,
-              width: 32
+              width: 32,
             }}
           >
             <Logo />
           </Box>
           <Box
             sx={{
-              alignItems: 'center',
-              backgroundColor: 'rgba(255, 255, 255, 0.04)',
+              alignItems: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.04)",
               borderRadius: 1,
-              cursor: 'pointer',
-              display: 'flex',
-              justifyContent: 'space-between',
+              cursor: "pointer",
+              display: "flex",
+              justifyContent: "space-between",
               mt: 2,
-              p: '12px'
+              p: "12px",
             }}
           >
             <div>
-              <Typography
-                color="inherit"
-                variant="subtitle1"
-              >
-                Devias
+              <Typography color="inherit" variant="subtitle1">
+                Booking Website
               </Typography>
-              <Typography
-                color="neutral.400"
-                variant="body2"
-              >
-                Production
+              <Typography color="neutral.400" variant="body2">
+                DHD
               </Typography>
             </div>
-            <SvgIcon
-              fontSize="small"
-              sx={{ color: 'neutral.500' }}
-            >
+            <SvgIcon fontSize="small" sx={{ color: "neutral.500" }}>
               <ChevronUpDownIcon />
             </SvgIcon>
           </Box>
         </Box>
-        <Divider sx={{ borderColor: 'neutral.700' }} />
+        <Divider sx={{ borderColor: "neutral.700" }} />
         <Box
           component="nav"
           sx={{
             flexGrow: 1,
             px: 2,
-            py: 3
+            py: 3,
           }}
         >
           <Stack
             component="ul"
             spacing={0.5}
             sx={{
-              listStyle: 'none',
+              listStyle: "none",
               p: 0,
-              m: 0
+              m: 0,
             }}
           >
             {items.map((item) => {
-              const active = item.path ? (pathname === item.path) : false;
+              const active = item.path ? pathname === item.path : false;
 
               return (
                 <SideNavItem
@@ -123,56 +113,23 @@ export const SideNav = (props) => {
             })}
           </Stack>
         </Box>
-        <Divider sx={{ borderColor: 'neutral.700' }} />
+        <Divider sx={{ borderColor: "neutral.700" }} />
         <Box
           sx={{
             px: 2,
-            py: 3
+            py: 3,
+            listStyle: "none",
           }}
+          onClick={handleSignOut}
         >
-          <Typography
-            color="neutral.100"
-            variant="subtitle2"
-          >
-            Need more features?
-          </Typography>
-          <Typography
-            color="neutral.500"
-            variant="body2"
-          >
-            Check out our Pro solution template.
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              mt: 2,
-              mx: 'auto',
-              width: '160px',
-              '& img': {
-                width: '100%'
-              }
-            }}
-          >
-            <img
-              alt="Go to pro"
-              src="/assets/devias-kit-pro.png"
-            />
-          </Box>
-          <Button
-            component="a"
-            endIcon={(
+          <SideNavItem
+            icon={
               <SvgIcon fontSize="small">
-                <ArrowTopRightOnSquareIcon />
+                <LogoutIcon />
               </SvgIcon>
-            )}
-            fullWidth
-            href="https://material-kit-pro-react.devias.io/"
-            sx={{ mt: 2 }}
-            target="_blank"
-            variant="contained"
-          >
-            Pro Live Preview
-          </Button>
+            }
+            title="Đăng xuất"
+          />
         </Box>
       </Box>
     </Scrollbar>
@@ -185,10 +142,10 @@ export const SideNav = (props) => {
         open
         PaperProps={{
           sx: {
-            backgroundColor: 'neutral.800',
-            color: 'common.white',
-            width: 280
-          }
+            backgroundColor: "neutral.800",
+            color: "common.white",
+            width: 280,
+          },
         }}
         variant="permanent"
       >
@@ -204,10 +161,10 @@ export const SideNav = (props) => {
       open={open}
       PaperProps={{
         sx: {
-          backgroundColor: 'neutral.800',
-          color: 'common.white',
-          width: 280
-        }
+          backgroundColor: "neutral.800",
+          color: "common.white",
+          width: 280,
+        },
       }}
       sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
       variant="temporary"
@@ -219,5 +176,5 @@ export const SideNav = (props) => {
 
 SideNav.propTypes = {
   onClose: PropTypes.func,
-  open: PropTypes.bool
+  open: PropTypes.bool,
 };
