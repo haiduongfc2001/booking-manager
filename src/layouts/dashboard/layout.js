dfa17002737792f7543ebe80dddbb02a04ec8a9b
haiduongfc2001 <haiduongfc2001@gmail.com>
2024-02-09 22:36:17 +0700

    dhd-014: hide/show side-bar with large sreen

--- src/layouts/dashboard/layout.js
+++ src/layouts/dashboard/layout.js
@@ -1,61 +1,53 @@
-import { useCallback, useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-import { styled } from '@mui/material/styles';
-import { withAuthGuard } from 'src/hocs/with-auth-guard';
-import { SideNav } from './side-nav';
-import { TopNav } from './top-nav';
+import { useCallback, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { styled } from "@mui/material/styles";
+import { withAuthGuard } from "src/hocs/with-auth-guard";
+import { SideNav } from "./side-nav";
+import { TopNav } from "./top-nav";
 
 const SIDE_NAV_WIDTH = 280;
 
-const LayoutRoot = styled('div')(({ theme }) => ({
-  display: 'flex',
-  flex: '1 1 auto',
-  maxWidth: '100%',
-  [theme.breakpoints.up('lg')]: {
-    paddingLeft: SIDE_NAV_WIDTH
-  }
+const LayoutRoot = styled("div")(({ theme, open }) => ({
+  display: "flex",
+  flex: "1 1 auto",
+  maxWidth: "100%",
+  [theme.breakpoints.up("lg")]: {
+    paddingLeft: open && SIDE_NAV_WIDTH,
+  },
 }));
 
-const LayoutContainer = styled('div')({
-  display: 'flex',
-  flex: '1 1 auto',
-  flexDirection: 'column',
-  width: '100%'
+const LayoutContainer = styled("div")({
+  display: "flex",
+  flex: "1 1 auto",
+  flexDirection: "column",
+  width: "100%",
 });
 
 export const Layout = withAuthGuard((props) => {
   const { children } = props;
   const pathname = usePathname();
-  const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = useState(true);
 
-  const handlePathnameChange = useCallback(
-    () => {
-      if (openNav) {
-        setOpenNav(false);
-      }
-    },
-    [openNav]
-  );
+  // const handlePathnameChange = useCallback(() => {
+  //   if (openNav) {
+  //     setOpenNav(false);
+  //   }
+  // }, [openNav]);
 
-  useEffect(
-    () => {
-      handlePathnameChange();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [pathname]
-  );
+  // useEffect(
+  //   () => {
+  //     handlePathnameChange();
+  //   },
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   [pathname]
+  // );
 
   return (
     <>
-      <TopNav onNavOpen={() => setOpenNav(true)} />
-      <SideNav
-        onClose={() => setOpenNav(false)}
-        open={openNav}
-      />
-      <LayoutRoot>
-        <LayoutContainer>
-          {children}
-        </LayoutContainer>
+      <TopNav onNavOpen={() => setOpenNav(true)} onClose={() => setOpenNav(false)} open={openNav} />
+      <SideNav onClose={() => setOpenNav(false)} open={openNav} />
+      <LayoutRoot open={openNav}>
+        <LayoutContainer>{children}</LayoutContainer>
       </LayoutRoot>
     </>
   );
